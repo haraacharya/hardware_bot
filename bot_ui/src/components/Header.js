4cd975bec1a63c3be89c4791df25d4c9d560d45d
Hara <hara.acharya@gmail.com>
2020-06-06 14:26:06 -0700

    added chatbot component

--- bot_ui/src/components/Header.js
+++ bot_ui/src/components/Header.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+
 const Header = () => {
     return (
+       <div>
        
-        <nav className="red" style={{padding: "0px 10px"}}>
-            <div className="nav-wraper">
-                <Link to={'/'} className="brand-logo">FunFactor</Link>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><Link to='/home'>Home</Link></li>
-                    <li><Link to='/about'>About</Link></li>
-                </ul>
-            </div>
-        </nav>
+            <nav className="red" style={{padding: "0px 10px"}}>
+                <div className="nav-wraper">
+                    <Link to={'/'} className="brand-logo">FunFactor</Link>
+                    <Link to="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><Link to='/home'>Home</Link></li>
+                        <li><Link to='/about'>About</Link></li>
+                    </ul>
+                </div>
+            </nav>
+            <ul id="mobile-demo" className="sidenav">
+                <li><Link to='/home'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+            </ul>
+        </div>
        
     )
 }
+
+
 
 export default Header;
--- bot_ui/src/components/chatbot/Chatbot.js
+++ bot_ui/src/components/chatbot/Chatbot.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios/index';
 
-const Chatbot = () => {
-    return (
-        <div>hardwarebot goes here</div>
-    )
+class Chatbot extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            messages: []
+        }
+    }
+
+    async df_text_query(text) {
+        let says = {
+            speaks: 'me',
+            msg: {
+                test: {
+                    text: text
+                }
+            }
+        };
+        this.setState({messages: [...this.state.messages, says]})
+        const res = await axios.post('/df/df_text_query', {text: text})
+
+        for (let msg of res.data.fullfillmentMessages) {
+            says = {
+                speaks: 'bot', 
+                msg: msg
+            }
+            this.setState({messages: [...this.state.messages, says]})
+        }
+
+    }
+
+    async df_event_query(event){
+
+    }
+
+    render() {
+        return (
+            <div style = {{height: 400, width:400, float: 'right'}} >
+                <div id='charbot' style = {{ height: '100%', width: '100%', overflow: 'auto'}}>
+                    <h2>chatbot</h2>
+                    <input type='text'/>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Chatbot;
